270f5a9f38f2045ae0a64360ab530e6ff7a78a8b
fmfahath <fmfahath.dev@gmail.com>
2025-05-08 15:05:56 +0530

    frontend -- Gemini API key created, @google/genai npm installed,  gemini.js and .env created in config folder

---
+++ src/config/gemini.js
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+
+
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+
+async function main(prompt) {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: prompt,
+    });
+    console.log(response.text);
+}
+
+export default main;
