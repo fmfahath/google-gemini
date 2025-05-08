007da4b34e6a73eb6e9b8adaed72781e1a23f52d
fmfahath <fmfahath.dev@gmail.com>
2025-05-08 15:35:31 +0530

    frontend -- ContextProvider created (Context.jsx)

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,7 +1,8 @@
 import { GoogleGenAI } from "@google/genai";
 
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
-const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: API_KEY });
 
 async function main(prompt) {
     const response = await ai.models.generateContent({
---
+++ src/context/Context.jsx
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import main from "../config/gemini";
+
+
+export const Context = createContext()
+
+const ContextProvider = (props) => {
+
+    const onSent = async (prompt) => {
+        await main(prompt)
+    }
+
+    onSent("what is gemini")
+
+    const contextValue = {
+
+    }
+
+    return (
+        <Context.Provider value={contextValue}>
+            {props.children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProvider;
