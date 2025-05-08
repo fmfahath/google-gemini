e6190cac32084783c518f21196ae9e0587d2c080
fmfahath <fmfahath.dev@gmail.com>
2025-05-08 13:11:46 +0530

    frontend -- search-box content added in main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -9,11 +9,13 @@ const Main = () => {
                 <p>Gemini</p>
                 <img src={assets.user_icon} alt="" />
             </div>
+
             <div className="main-container">
                 <div className="greet">
-                    <p><span>Hello, Fahat</span></p>
+                    <p><span>Hello, Fahath</span></p>
                     <p>How can I help you today?</p>
                 </div>
+
                 <div className="cards">
                     <div className="card">
                         <p>Suggest Some places to see on an upcoming road trip</p>
@@ -31,6 +33,18 @@ const Main = () => {
                         <p>Suggest Some places to see on an upcoming road trip</p>
                         <img src={assets.code_icon} alt="" />
                     </div>
+                </div>
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className='bottom-info'>*Gemini can make mistakes. Check important info.</p>
                 </div>
             </div>
         </div>
